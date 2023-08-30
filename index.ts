cbeb4c70fb7b1e0ffaf02eaf26c6e03554bfec44
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-31 00:46:51 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,23 +1,21 @@
 import { fromEvent, pipe } from 'rxjs';
 import { bufferToggle, map } from 'rxjs/operators';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/transformation/buffertoggle
+// Example 2: Toggle buffer on and off on mouse down/up
 console.clear();
 
 const getXYCoordinates = pipe(
-  map<[MouseEvent], {}>(e => e.map(v => ({ x: v.clientX, y: v.clientY }))),
-  map(e => JSON.stringify(e))
-)
+  map<[MouseEvent], {}>((e) => e.map((v) => ({ x: v.clientX, y: v.clientY }))),
+  map((e) => JSON.stringify(e))
+);
 
 const buffer = pipe(
-  bufferToggle(
-    fromEvent(document, 'mousedown'),
-    _ => fromEvent(document, 'mouseup')
+  bufferToggle(fromEvent(document, 'mousedown'), (_) =>
+    fromEvent(document, 'mouseup')
   )
-)
+);
 
 fromEvent(document, 'mousemove')
-  .pipe(
-    buffer,
-    getXYCoordinates
-  )
-  .subscribe(console.log)
+  .pipe(buffer, getXYCoordinates)
+  .subscribe(console.log);
